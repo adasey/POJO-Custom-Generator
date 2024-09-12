d610aced1fe0bcae7c8aca878e14f027e1227bee
mulee <mulee@e-hcg.com>
2024-09-12 18:06:38 +0900

    [set] JUNIT test import for test

---
+++ pojo-generator/src/main/test/fr/jukien/intellij/plugins/action/DTOTest.java
@@ -0,0 +1,18 @@
+package fr.jukien.intellij.plugins.action;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DTOTest {
+
+  @org.junit.jupiter.api.Test
+  void actionPerformed() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void update() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getActionUpdateThread() {
+  }
+}
